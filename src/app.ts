5b85e6911fdcbe2c75315ca54768e166ee2fac2e
David L Whitehurst <me@dlwhitehurst.com>
2024-03-14 17:26:58 -0400

    added swagger to all

--- src/app.ts
+++ src/app.ts
@@ -26,6 +26,10 @@ import memberRouter from "./routes/member.routes";
 import cookieSession from "cookie-session";
 import * as process from "process";
 
+// swagger api documentation
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
+
 const initIndex = process.argv.indexOf("--init=true");
 const init = initIndex !== -1;
 
@@ -58,6 +62,9 @@ app.use(function (_req, res, next) {
 app.get("/", (_req, res) => {
     res.send("Welcome to the VirtualYou Business API.");
 });
+
+// swagger path to api documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // database
 const Member = db.member;
